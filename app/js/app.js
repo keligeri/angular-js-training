72618bb5fc2c8fc3b4d79de4055843b7b9ccaaab
Kelemen <503023779@logon.ds.ge.com>
2017-12-14 15:54:01 +0100

    Done with config, printed hello World to base route

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,4 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-git che
-])
-    .config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
-    $locationProvider.hashPrefix('!');
-
-    $routeProvider.otherwise({redirectTo: '/myview'});
-}]);
+var app = angular.module('myApp', [ ]);
